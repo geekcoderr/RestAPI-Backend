ac3a042eecdd576c39de206336145f82ccee46f1
Nishant Maheshwari <mnishant222@gmail.com>
2024-03-24 00:21:37 +0530

    some more methods

--- index.js
+++ index.js
@@ -16,4 +16,22 @@ app.get('/api/users',(req,res)=>{
     return  res.json(users);
 });
 
+app.get('/api/users/id/:id', (req, res) => {
+    const id = Number(req.params.id);
+    console.log('Requested ID:', id);
+    const user = users.find((user) => user.id === id);
+    console.log('User found:', user);
+    return res.json(user);
+});
+
+app.get('/api/users/name/:name', (req, res) => {
+    const name = req.params.name;
+    console.log('Requested name:', name);
+    const lowercaseName = name.toLowerCase();
+    const user = users.find((user) => user.first_name.toLowerCase() === lowercaseName);
+    console.log('User found:', user);
+    return res.json(user);
+});
+
+
 app.listen(PORT,console.log('Server started'));
